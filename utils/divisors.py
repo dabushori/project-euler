13251631f2e59b7624d1e38d5fefc99217c449f2
Ori Dabush <dabushori@gmail.com>
2024-01-11 17:56:54 +0200

    Fixed a bug in the get_divisors function

--- utils/divisors.py
+++ utils/divisors.py
@@ -1,10 +1,10 @@
-from math import ceil
+from math import floor
 
 
 def get_divisors(number: int):
     if number <= 0:
         raise ValueError('Invalid number')
-    for candidate in range(1, ceil(number ** 0.5 + 1)):
+    for candidate in range(1, floor(number ** 0.5) + 1):
         if number % candidate == 0:
             yield candidate
             other_candidate = number // candidate
