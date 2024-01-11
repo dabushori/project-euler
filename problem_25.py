1f2b9c3f3ceabc985ce515b0f6b3df09f4107fc5
Ori Dabush <dabushori@gmail.com>
2024-01-11 18:35:09 +0200

    Added solution for problem 25

---
+++ problem_25.py
@@ -0,0 +1,24 @@
+"""
+Challenge 25 of project euler - 1000-digit Fibonacci Number
+
+@author Ori Dabush
+"""
+
+from utils.fibonacci import fibonacci_from
+
+
+NUMBER_OF_DIGITS = 1000
+LOWER_LIMIT = 10 ** (NUMBER_OF_DIGITS - 1)
+
+
+def solve():
+    index, _ = next(fibonacci_from(LOWER_LIMIT, return_indexes=True))
+    return index
+
+
+def main():
+    print(f'The answer is {solve()}')
+
+
+if __name__ == '__main__':
+    main()
--- utils/fibonacci.py
+++ utils/fibonacci.py
@@ -1,7 +1,20 @@
-def fibonacci_until(upper_limit):
+def fibonacci_until(upper_limit: int, return_indexes: bool = False):
     previous_element, current_element = 0, 1
+    index = 1
     while True:
         if current_element > upper_limit:
             break
-        yield current_element
+
+        yield index, current_element if return_indexes else current_element
         previous_element, current_element = current_element, previous_element + current_element
+        index += 1
+
+
+def fibonacci_from(lower_limit: int, return_indexes: bool = False):
+    previous_element, current_element = 0, 1
+    index = 1
+    while True:
+        if current_element >= lower_limit:
+            yield index, current_element if return_indexes else current_element
+        previous_element, current_element = current_element, previous_element + current_element
+        index += 1
