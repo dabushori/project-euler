d60fae7541d498379581c8d360bf29f13ddab311
Ori Dabush <dabushori@gmail.com>
2024-01-13 11:03:01 +0200

    Added solution for problem 41

---
+++ problem_41.py
@@ -0,0 +1,24 @@
+"""
+Challenge 41 of project euler - Pandigital Prime
+
+@author Ori Dabush
+"""
+
+from utils.primes import is_prime
+from utils.pandigitals import get_pandigital_numbers
+
+
+def solve():
+    for n in range(9, 0, -1):
+        pandigital_numbers = sorted(get_pandigital_numbers(n), reverse=True)
+        for candidate in pandigital_numbers:
+            if is_prime(candidate):
+                return candidate
+
+
+def main():
+    print(f'The answer is {solve()}')
+
+
+if __name__ == '__main__':
+    main()
--- utils/pandigitals.py
+++ utils/pandigitals.py
@@ -4,3 +4,19 @@ DIGITS = set(map(str, range(1, 10)))
 def is_one_to_nine_pandigital(number: int | str):
     number = str(number)
     return len(number) == len(DIGITS) and set(number) == DIGITS
+
+
+def _get_pandigital_numbers_custom_digits(digits: list[int]):
+    if len(digits) < 1:
+        raise ValueError("Invalid digits argument")
+    elif len(digits) == 1:
+        yield digits[0]
+    else:
+        for index, digit in enumerate(digits):
+            remaining_digits = digits[:index] + digits[index + 1:]
+            for remainder in _get_pandigital_numbers_custom_digits(remaining_digits):
+                yield int(f'{digit}{remainder}')
+
+
+def get_pandigital_numbers(max_n: int):
+    yield from _get_pandigital_numbers_custom_digits(list(range(1, max_n + 1)))
