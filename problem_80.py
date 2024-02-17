5ff1bb713ed47bcb56fd3a7c97269623b1ab30b7
Ori Dabush <dabushori@gmail.com>
2024-02-17 12:34:57 +0200

    Added solution for problem 80

---
+++ problem_80.py
@@ -0,0 +1,34 @@
+"""
+Challenge 80 of project euler - Square Root Digital Expansion
+
+@author Ori Dabush
+"""
+
+from decimal import Decimal, getcontext
+
+
+NUMBER_COUNT = 100
+DIGIT_PERCISION = 100
+
+
+def sum_decimal_digits(f: Decimal) -> int:
+    f = str(f).replace('.', '')
+    return sum(map(int, f[:DIGIT_PERCISION]))
+
+
+def solve():
+    getcontext().prec = DIGIT_PERCISION + 1
+    result = 0
+    for number in range(1, NUMBER_COUNT + 1):
+        if Decimal(number).sqrt() == int(Decimal(number).sqrt()):
+            continue
+        result += sum_decimal_digits(Decimal(number).sqrt())
+    return result
+
+
+def main():
+    print(f'The answer is {solve()}')
+
+
+if __name__ == '__main__':
+    main()
