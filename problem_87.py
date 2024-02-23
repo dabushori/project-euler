7214a025a90b17a1689b1e153233fbc9573e8c1a
Ori Dabush <dabushori@gmail.com>
2024-02-23 21:24:03 +0200

    Added solution for problem 87

---
+++ problem_87.py
@@ -0,0 +1,39 @@
+"""
+Challenge 87 of project euler - Prime Power Triples
+
+@author Ori Dabush
+"""
+
+from utils.primes import prime_numbers_until_with_space
+from math import ceil, sqrt
+from itertools import product
+
+
+MAX_SUM = 50_000_000
+
+
+def solve():
+    """
+    We know that:
+    p**2 + q**3 + r**4 <= MAX_SUM
+
+    so we can derive that the primes we will use will be less than sqrt(MAX_SUM).
+    """
+    numbers = set()
+    primes = set(prime_numbers_until_with_space(ceil(sqrt(MAX_SUM))))
+    squares = [p**2 for p in primes]
+    cubes = [p**3 for p in primes]
+    fourths = [p**4 for p in primes]
+    for p2, q3, r4 in product(squares, cubes, fourths):
+        n = p2 + q3 + r4
+        if n <= MAX_SUM:
+            numbers.add(n)
+    return len(numbers)
+
+
+def main():
+    print(f'The answer is {solve()}')
+
+
+if __name__ == '__main__':
+    main()
