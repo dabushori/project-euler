5489d0342267b081812406cc815dc3f7b7b124cc
Ori Dabush <dabushori@gmail.com>
2025-01-28 17:56:48 +0200

    Solved problem 104

--- utils/fibonacci.py
+++ utils/fibonacci.py
@@ -18,3 +18,15 @@ def fibonacci_from(lower_limit: int, return_indexes: bool = False):
             yield (index, current_element) if return_indexes else current_element
         previous_element, current_element = current_element, previous_element + current_element
         index += 1
+
+
+def fibonacci_from_index(start_index: int, return_indexes: bool = False):
+    previous_element, current_element = 0, 1
+    index = 1
+    while index < start_index:
+        previous_element, current_element = current_element, previous_element + current_element
+        index += 1
+    while True:
+        yield (index, current_element) if return_indexes else current_element
+        previous_element, current_element = current_element, previous_element + current_element
+        index += 1
