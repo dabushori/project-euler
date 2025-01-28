0e8427a01ef8e40a6168cbddcbce46a2eb5260ef
Ori Dabush <dabushori@gmail.com>
2025-01-28 17:46:28 +0200

    Added a feature to fetch the challenge description from the website automatically

--- template/generate_challenge.py
+++ template/generate_challenge.py
@@ -3,9 +3,11 @@ Generate a challenge solution script from the template
 
 @author Ori Dabush
 """
+
 from argparse import ArgumentParser
 from os.path import join, dirname
-
+from bs4 import BeautifulSoup
+import requests
 
 SOLUTION_FILE_NAME = 'solution.py'
 SOLUTION_TEMPLATE_FILE_NAME = join(dirname(__file__), 'template.py')
@@ -14,8 +16,17 @@ SOLUTION_TEMPLATE_FILE_NAME = join(dirname(__file__), 'template.py')
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('challenge_number', type=int)
-    parser.add_argument('challenge_description', type=str)
-    return parser.parse_args()
+    parser.add_argument('-d', '--description', type=str, required=False)
+    args = parser.parse_args()
+    if args.description is None:
+        # Fetch the description from the website if not given
+        url = f'https://projecteuler.net/problem={args.challenge_number}'
+        response = requests.get(url, allow_redirects=False)
+        if response.status_code != 200:
+            raise Exception(f'Failed fetching challenge description: {response} ({response.content})')
+        html_parser = BeautifulSoup(response.content.decode(), 'html.parser')
+        args.description = html_parser.find('h2').text
+    return args
 
 
 def main():
@@ -23,7 +34,7 @@ def main():
     with open(SOLUTION_TEMPLATE_FILE_NAME, 'r') as template_file:
         template = template_file.read()
     with open(f'problem_{args.challenge_number}.py', 'w') as solution_script:
-        solution_script.write(template.format(args.challenge_number, args.challenge_description))
+        solution_script.write(template.format(args.challenge_number, args.description))
 
 
 if __name__ == '__main__':
