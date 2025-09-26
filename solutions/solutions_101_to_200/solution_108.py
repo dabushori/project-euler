bd17940385ab6fd2cc702083cca50b68bf3c9426
Ori Dabush <dabushori@gmail.com>
2025-09-26 23:12:21 +0300

    Added my attempt for 108

--- solutions/solutions_101_to_200/solution_108.py
+++ solutions/solutions_101_to_200/solution_108.py
@@ -6,7 +6,6 @@ Challenge 108 of project euler - Diophantine Reciprocals I
 
 from itertools import count
 
-
 MIN_SOLUTIONS = 1000
 
 
@@ -18,6 +17,17 @@ n = xy/(x+y)
 
 1/y = 1/n - 1/x = (x - n) / xn
 y = xn / (x - n)
+
+
+
+1/x + 1/y = 1/n
+(x+y)/xy=1/n
+n=xy/(x+y)
+
+We know that:
+x|yn
+y|xn
+n|xy
 """
 
 
@@ -26,27 +36,23 @@ def count_distinct_solutions(n: int) -> int:
     Count distinct solutions to 1/x + 1/y = 1/n, where x, y and n are positive integers.
     """
     distinct_solutions = 0
-    for factor in count(2):
-        # y = x * n / (x - n) = n * factor * n / (n * factor - n) = factor * n / (factor - 1)
-        y_nominator = factor * n
-        y_denominator = factor - 1
-        y = y_nominator // y_denominator
-        if y <= n:
-            break
-        if y_nominator % y_denominator == 0:
-            y = y_nominator // y_denominator
+    min_possible_x, max_possible_x = n + 1, 2 * n
+    print(f'The value of n is {n}')
+    for x in range(min_possible_x, max_possible_x + 1):
+        y_n, y_d = x*n, x-n
+        if y_n % y_d == 0: 
             distinct_solutions += 1
+            print(f'1 / {x} + 1 / {y_n//y_d} = 1 / {n}')
     return distinct_solutions
 
 
 def solve():
-    for n in range(2, 100):
-        print(f'count_distinct_solutions({n}) = {count_distinct_solutions(n)}')
-    # for n in count(1):
-    #     distinct_solutions = count_distinct_solutions(n)
-    #     print(f'{n}: {distinct_solutions}')
-    #     if distinct_solutions > MIN_SOLUTIONS:
-    #         return n
+    for n in count(1):
+        distinct_solutions = count_distinct_solutions(n)
+        if n % 1000 == 0:
+            print(f'{n}: {distinct_solutions}')
+        if distinct_solutions > MIN_SOLUTIONS:
+            return n
 
 
 def main():
